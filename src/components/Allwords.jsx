389a0cce212eb36d9970b1b1dca981667c8db308
preetiag18 <preetiag18@gmail.com>
2022-12-06 15:16:16 +0200

    add header component

---
+++ src/components/Allwords.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Allwords = () => {
+    return (
+        <div>
+            There should be Allwords
+            
+        </div>
+    );
+};
+
+export default Allwords;
