ea2c934545023ab456ae89738be38b18892c12e2
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-12-06 15:31:40 +0200

    add space

--- src/components/Allwords.jsx
+++ src/components/Allwords.jsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Allwords = () => {
-    return (
-        <div>
-            There should be Allwords
-            
-        </div>
-    );
+  return <div>There should be All words</div>;
 };
 
 export default Allwords;
