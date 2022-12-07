df27fc591769e72639087ac90274f4d8020e2685
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-12-07 23:22:59 +0200

    add temporary styling to blobs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import classes from "./Hero.module.css";
 const Hero = () => {
   return (
     <div className={classes.hero}>
-      <h1>Learn Finnish Words</h1>
+      <h1>Learn Finnish words</h1>
       <div className={classes.calls_to_action}>
         <button>
           Where should I start? <i class="fa-solid fa-arrow-right"></i>
@@ -14,12 +14,22 @@ const Hero = () => {
           Why Sanasauna? <i class="fa-solid fa-arrow-right"></i>
         </button>
       </div>
-      <div className={classes.blobname}>
-        <h3>Family</h3>
-        <h3>Animals</h3>
-        <h3>Food</h3>
-        <h3>Colours</h3>
-        <h3>Months</h3>
+      <div className={classes.blobs}>
+        <div className={classes.blob}>
+          <h3>Family</h3>
+        </div>
+        <div className={classes.blob}>
+          <h3>Animals</h3>
+        </div>
+        <div className={classes.blob}>
+          <h3>Food</h3>
+        </div>
+        <div className={classes.blob}>
+          <h3>Colours</h3>
+        </div>
+        <div className={classes.blob}>
+          <h3>Months</h3>
+        </div>
       </div>
     </div>
   );
