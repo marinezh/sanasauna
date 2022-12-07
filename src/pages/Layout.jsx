8a1ea29527f8678d9b98398887116c8b15b697b9
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-12-07 22:39:11 +0200

    stick footer to bottom of the page

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import classes from "./Layout.module.css";
 
 const Layout = () => {
   return (
-    <div>
+    <div className={classes.layout}>
       <Header />
       <Main />
       <Footer />
