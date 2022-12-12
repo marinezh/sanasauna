e8a12e4035d6ca751b4b6bb39188b888d278b194
Marina <78799773+marinezh@users.noreply.github.com>
2022-12-12 13:44:31 +0200

    add storage folder , start working on server part

---
+++ src/storage/storageLayer.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const path = require("path");
+
+const { key, adapterFile, storageFile } = require("./storageConfig.json");
+
+const { readStorage, writeStorage } = require("./readerWriter");
+
+const storageFilePath = path.join(__dirname, storageFile);
+
+console.log(storageFilePath);
+
+const { adapt } = require(path.join(__dirname, adapterFile));
+
+async function getAllFromStorage() {
+  return readStorage(storageFilePath);
+}
+
+getAllFromStorage().then(console.log).catch(console.log);
