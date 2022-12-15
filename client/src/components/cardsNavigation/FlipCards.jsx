97ad79b856de3abb36a832bcc655d8c575543c26
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-12-15 10:32:20 +0200

    add flip all to ___ buttons to flip cards

--- client/src/components/cardsNavigation/FlipCards.jsx
+++ client/src/components/cardsNavigation/FlipCards.jsx
@@ -6,10 +6,20 @@ import classes from "./FlipCards.module.css";
 
 const FlipCards = ({ data }) => {
   return (
-    <div className={classes.cards}>
-      {data.map((card) => (
-        <Card key={card.name} name={card.name} translation={card.translation} />
-      ))}
+    <div className={classes.cards_container}>
+      <div className={classes.cards}>
+        {data.map((card) => (
+          <Card
+            key={card.name}
+            name={card.name}
+            translation={card.translation}
+          />
+        ))}
+      </div>
+      <div className={classes.buttons}>
+        <button>Flip all to English</button>
+        <button>Flip all to Finnish</button>
+      </div>
     </div>
   );
 };
