f1827454771235915d83d87568af0eda55b7deb2
preetiag18 <preetiag18@gmail.com>
2023-04-17 22:35:10 +0300

    move firebase config to env variable

--- client/src/auth/firebase.js
+++ client/src/auth/firebase.js
@@ -15,14 +15,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "sanasauna-af75f.firebaseapp.com",
-  databaseURL:
-    "https://sanasauna-af75f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sanasauna-af75f",
-  storageBucket: "sanasauna-af75f.appspot.com",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
   messagingSenderId: "148695523951",
-  appId: "1:148695523951:web:0f1180771f81a40779e39a",
+  appId: `${process.env.REACT_APP_APP_ID}`,
   measurementId: "G-ENTW1SGT6D",
 };
 
